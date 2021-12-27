e1df14f925ca340a4fc60d14f50199a2e303b2cc
ElmerPC <erey2790@gmail.com>
2021-12-26 21:26:09 -0600

    adding fetch

---
+++ assets/css/js/script.js
@@ -0,0 +1,8 @@
+var apiKey = "3eb18e0a949449fa503ddba9cdea5c69";
+var apiUrl = "api.openweathermap.org/data/2.5/weather?id={city}&appid=eb18e0a949449fa503ddba9cdea5c69"
+
+var getUserRepos = function() {
+    fetch()
+};
+
+getUserRepos();
